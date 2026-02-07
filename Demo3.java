e1c7074ab0fb49cdd97905cf8ab73b4716145219
Zakiya-26 <zakhiyamdzakhiya@gmail.com>
2026-02-07 10:56:10 +0530

    Add files via upload

---
+++ Demo3.java
@@ -0,0 +1,18 @@
+public class Demo3 {
+   
+    public static void main(String[] args) {
+       String Studentname="konahamaru";
+       int age=18;
+       int grade='A';
+       int marks=951;
+       String Status="pass";
+       System.out.println("Student name:"+Studentname);
+       System.err.println("Student age:"+age);
+       System.out.println("grade:"+grade);
+       System.out.println("marks:"+marks);
+       System.out.println("status:"+Status);
+       
+
+
+    }
+}
