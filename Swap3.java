cfc6af872812a6e0e3c07c24957f0bd54b40d20d
Zakiya-26 <zakhiyamdzakhiya@gmail.com>
2026-02-07 15:19:26 +0530

    Add files via upload

---
+++ Swap3.java
@@ -0,0 +1,15 @@
+public class Swap3 {
+    public static void main(String[] args){
+        int a=10;
+        int b=50;
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println(a);
+        System.out.println(b);
+        
+
+
+    }
+    
+}
