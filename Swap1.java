443ec2db3c679d0bcd4088263523e9a42f34c9c8
Zakiya-26 <zakhiyamdzakhiya@gmail.com>
2026-02-07 15:18:47 +0530

    Add files via upload

---
+++ Swap1.java
@@ -0,0 +1,15 @@
+public class Swap1 {
+    public static void main(String[] args){
+        int a= 23;
+        int b=45;
+        System.out.println("before swaping a value:"+ a +"b value:"+b);
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println("after swaping a value:"+ a +"b value:"+b);
+
+    }
+    
+}
