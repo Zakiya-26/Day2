8e67c940200c2b2fe67b85f52a60ce9fc5034301
Zakiya-26 <zakhiyamdzakhiya@gmail.com>
2026-02-07 11:03:25 +0530

    Add files via upload

---
+++ Demo4.java
@@ -0,0 +1,17 @@
+public class Demo4 {
+    public static void main(String[] args){
+        String name="naruto";
+        int telugu=56;
+        int math=89;
+        int bio=100;
+        int sum=telugu+math+bio;
+        double average=sum/3;
+        System.out.println("name:"+name);
+        System.out.println("----marks----");
+        System.out.println("telugu:"+telugu);
+        System.out.println("math:"+math);
+        System.out.println("bio:"+bio);
+        System.out.println("sum:"+sum);
+        System.out.println("average:"+average);
+    } 
+}
