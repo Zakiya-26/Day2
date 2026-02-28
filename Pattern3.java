44eb317868ad12090c2cedf5b1c022cf960597f2
Zakiya-26 <zakhiyamdzakhiya@gmail.com>
2026-02-28 14:40:54 +0530

    Add files via upload

---
+++ Pattern3.java
@@ -0,0 +1,21 @@
+public class Pattern3 {
+    public static void main(String[] args) {
+        int n=9;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(j==1||j==n||i==j||i+j==n+1){
+                    System.out.print("*");
+
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+
+        }
+    
+
+    }
+    
+}
