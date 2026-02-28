ad6789dbdef72750785176f70e754aceff2aee1e
Zakiya-26 <zakhiyamdzakhiya@gmail.com>
2026-02-28 13:49:43 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,25 @@
+   import java.util.*;
+   public class Array{
+        public static void main(String[] args){
+            Scanner sc=new Scanner(System.in);
+            System.out.println("enter array Size:");
+            int n=sc.nextInt();
+            int[] A=new int[n];
+             System.out.print("enter array elements:");
+            for(int i=0;i<n;i++){
+               
+                  A[i]=sc.nextInt();
+                 }
+                 System.out.println("Array Elements Are:");
+                 for(int i=0;i<n;i++){
+                    System.out.println(A[i]);
+                 }
+{
+
+}
+
+        }
+    }
+
+    
+
